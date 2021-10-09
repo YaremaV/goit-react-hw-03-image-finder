77386558ee639f4c483bea84f67b8a0cd3617ab9
YaremaV <Yaremenko1996v@gmail.com>
2021-10-09 17:04:33 +0300

    Updates

---
+++ static/js/main.0ea51f0d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var i=n(1),r=n.n(i),c=n(3),o=n.n(c),a=(n(8),n(0));var s=function(){return Object(a.jsx)("div",{children:"Hello"})};o.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.0ea51f0d.chunk.js.map
